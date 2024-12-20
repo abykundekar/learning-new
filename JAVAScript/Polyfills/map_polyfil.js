94a199f17c1cd761f3f30aeca2e209964501919b
Abhijeet <abhi@botzee.com>
2024-12-20 21:52:26 +0530

    map polyfil commented

--- JAVAScript/Polyfills/map_polyfil.js
+++ JAVAScript/Polyfills/map_polyfil.js
@@ -1,5 +1,10 @@
 let nums = [1, 2, 3, 4, 5];
 
+/**
+ * Takes in a callback and applies it to each element of the array it is called on
+ * @param {function} cb - callback function to be applied to each element of the array
+ * @returns {array} - new array with modified elements
+ */
 Array.prototype.myMap = function (cb) {
   console.log("this ", this);
   let result = [];
